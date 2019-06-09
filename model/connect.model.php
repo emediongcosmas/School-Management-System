74aea960412aa0b9618893e663845029894902a8
Emediong Cosmas <emediongcosmas1@gmail.com>
2019-06-09 19:37:58 +0100

    Add database connection model

---
+++ model/connect.model.php
@@ -0,0 +1,41 @@
+<?php
+
+    class Dbh {
+        
+        private $servername;
+        private $username;
+        private $password;
+        private $dbname;
+        private $charset;
+        
+        public function connect() {
+            
+            $this->servername = 'localhost';
+            $this->username = 'root';
+            $this->password = 'emediong1';
+            $this->dbname = 'studentportal';
+            $this->charset = 'latin1';
+            
+            try {
+                
+                $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+                $pdo = new PDO($dsn, $this->username, $this->password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                return $pdo;
+                
+                echo 'Done';
+                
+                } 
+            
+            catch (PDOException $e) {
+                    
+                echo "Connection failed:".$e->getMessage();
+                    
+                }
+            
+        }
+        
+    }
+
+?>
---
+++ model/student.model.php
@@ -0,0 +1,23 @@
+<?php
+
+include '../model/connect.model.php';
+
+class Student extends Dbh {
+    
+    private $surname;
+    private $firstname;
+    private $middlename;
+    private $dob;
+    private $email;
+    private $gender;
+    private $studentid;
+    private $address;
+    private $city;
+    private $state;
+    private $guardian_name;
+    private $guardian_no;
+    private $relationship;
+    
+}
+
+?>
